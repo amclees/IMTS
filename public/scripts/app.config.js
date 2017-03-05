a1c100d06096f7e62883555392dea8220930d5be
Andrew McLees <andrewmclees5@gmail.com>
2017-03-04 21:53:03 -0800

    Fixed source location issues

--- public/scripts/app.config.js
+++ public/scripts/app.config.js
@@ -1,3 +1,18 @@
+var authFunction = function($location, $cookies, $http) {
+  $http.post("/auth", { token: $cookies.get("token") })
+  .success(function(data, status, headers, config) {
+    if(!data.auth) {
+      $location.url("/login");
+      return;
+    }
+  })
+  .error(function(data, status, headers, config) {
+    console.log("Error during login request.");
+    $location.url("/login");
+    return;
+  });
+}
+
 angular.
   module("imts").
   config(["$locationProvider", "$routeProvider",
@@ -30,20 +45,14 @@ angular.
           templateUrl: "/templates/dashboard.template.html",
           controller: "dashboardCtrl",
           resolve: {
-            authed: function($location, $cookies, $http) {
-              $http.post("/auth", { token: $cookies.get("token") })
-                .success(function(data, status, headers, config) {
-                  if(!data.auth) {
-                    $location.url("/login");
-                    return;
-                  }
-                })
-                .error(function(data, status, headers, config) {
-                  console.log("Error during login request.");
-                  $location.url("/login");
-                  return;
-                });
-            }
+            authed: authFunction
+          }
+        }).
+        when("/test", {
+          templateUrl: "/templates/test.template.html",
+          controller: "testCtrl",
+          resolve: {
+            authed: authFunction
           }
         }).
         otherwise({
