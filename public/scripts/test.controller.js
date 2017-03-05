573c498010a4f68e7bc125425df03ce64036145a
Justin Liu <JNLiu5@gmail.com>
2017-03-05 07:38:57 -0800

    Updated controller

--- public/scripts/test.controller.js
+++ public/scripts/test.controller.js
@@ -1,15 +1,8 @@
-app.controller("testCtrl", function($scope, $http, $location, $cookies) {
-	$scope.slider = {
-	  	value: 50,
-	 	options: {
-	    	floor: 0,
-	    	ceil: 100
-  		}
-	};
+app.controller("testCtrl", function($scope, $http, $location, $cookies) {};
 	$scope.submit = function() {
 		var slider = $scope.slider.value;
 		var q1 = $scope.q1;
-		var answers = { "slider": slider, "q1": q1, "token": $cookies.get("token")};
+		var answers = { "work": $scope.work, "time": $scope.time, "leave": $scope.leave, "past": $scope.past, "nwhy": nwhy, "nwork": nwork, "support": support, "token": $cookies.get("token")};
 		$http.post("/test-submit", JSON.stringify(answers)).success(function(data, status, headers, config) {
 			console.log(data);
 		}).error(function(data, status, headers, config) {
