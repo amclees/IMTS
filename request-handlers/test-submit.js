c5ecf120176ffa285802f12ee00e2ebaa2897636
Andrew McLees <andrewmclees5@gmail.com>
2017-03-04 23:20:04 -0800

    Test submission base

--- request-handlers/test-submit.js
+++ request-handlers/test-submit.js
@@ -1,29 +1,42 @@
 var mongo = require("mongodb").MongoClient;
 
+var failure = "Submission Failed";
 module.exports = function(dbUrl) {
   return function(request, response) {
     var send = function(data) {
-      response.send("");
+      response.send(data);
     }
+    var body = request.body;
+    var token = body.token;
     mongo.connect(dbUrl, function(error, db) {
       var users = db.collection("users");
 
-      users.find({ "username": username, "passwordHash": passwordHash }).toArray(function(error, docs) {
+      var addResponse = function(user) {
+        var data = user.data;
+        var date = new Date();
+        data[date.toString()] = {
+          // Questions and answers
+        };
+        users.update({ "token": token }, { $set: { "data": data } });
         db.close();
+      }
+
+      users.find({ "token": token }).toArray(function(error, docs) {
         if(docs.length === 0) {
-          send(invalidAuth);
+          db.close();
+          send(failure);
           return;
         } else if(docs.length > 1) {
           console.log("Duplicate user in database");
+          db.close();
+          send(failure);
           return;
         } else {
-          hash = crypto.createHash("sha256");
-          hash.update(username + password);
-          send(hash.digest("hex"));
+          addResponse(docs[0]);
           return;
         }
       });
-      db.close();
+
     });
   }
 }
